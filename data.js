6be48867b53fc79982ffa343fc54e9a2b9dcfc95
Omran Haghegh <omranhaghegh@omrans-mbp.lan>
2019-06-21 02:19:20 -0600

    slidshow

---
+++ data.js
@@ -0,0 +1,13 @@
+export var food = {
+  burgers:[{name:"Illegal burgers", link:"https://www.illegalburger.com/"}],
+  pizza:[{name:"Pizza Republica", link:"https://pizzarepublica.com/"}],
+  Soups:[{name:"ChoLon", link:"https://www.cholon.com/denver/" }],
+  Pasta:[{name:"Olive Garden", link:"https://www.olivegarden.com/locations/location-search?redirectToMenu=true&cmpid=br:og_ag:360i_ch:ppc_pl:ad_ca:871204269_adg:42791136214_kw:olive+garden&redirectToTarget=true"}],
+  IceCream:[{name:"Cold Stone", link:"https://www.coldstonecreamery.com/"}],
+  Salads:[{name:"MAD Greens", link:"https://www.madgreens.com/"}],
+  Sandwhiches:[{name:"Leven Deli Co.", link:"https://eatleven.com/?utm_source=google&utm_medium=GMB&utm_campaign=Local"}],
+  seafood:[{name:"Revel Social", link:"https://revelsocialdenver.com/"}],
+  breakfest:[{name:"Olive & Finch", link:"https://www.oliveandfincheatery.com/"}],
+  Steak:[{name:"Morton's Steakhouse", link:"https://www.mortons.com/"}],
+  Taco:[{name:"Illegal Pete's LoDo", link:"https://www.illegalpetes.com/location/illegal-petes-denver-lower-downtown/"}]
+}
---
+++ main.js
@@ -0,0 +1,92 @@
+var pasta = document.getElementsByClassName(".pasta");
+var pizza = document.getElementsByClassName(".pizza");
+var soups = document.getElementsByClassName(".soups");
+var salads = document.getElementsByClassName(".salads");
+var burgers = document.getElementsByClassName(".burgers");
+var sandwhiches = document.getElementsByClassName(".sandwhiches");
+var ice-cream = document.getElementsByClassName(".ice-cream");
+var seafood = document.getElementsByClassName(".seafood");
+var breakfast = document.getElementsByClassName(".breakfast");
+var steak = document.getElementsByClassName(".steak");
+var tacos = document.getElementsByClassName(".tacos");
+// all the var that are grabbing
+//
+//
+// function myFood(){
+// if (pasta.checked == true) {
+//   pasta.addClass('show')
+//   pasta.removeClass('hide')
+//    } else {
+//      pasta.classList.add('hide')
+//      paste.classList.remove('show')
+// if (pizza.checked == true) {
+//   pizza.addClass('show')
+//   pizza.removeClass('hide')
+//    } else {
+//      pizza.addClass('hide')
+//      pizza.removeClass('show')}
+//
+//   if (soups.checked == true) {
+//     soups.addClass('show')
+//     soups.removeClass('hide')
+//      } else {
+//        soups.addClass('hide')
+//        soups.removeClass('show')}
+//
+//
+//   if (salads.checked == true) {
+//     salads.addClass('show')
+//     salads.removeClass('hide')
+//      } else {
+//        salads.addClass('hide')
+//        salads.removeClass('show')}
+//
+//     if (burgers.checked == true) {
+//       burgers.addClass('show')
+//       burgers.removeClass('hide')
+//        } else {
+//          burgers.addClass('hide')
+//          burgers.removeClass('show')}
+//
+// if (sandwhiches.checked == true) {
+//   sandwhiches.addClass('show')
+//   sandwhiches.removeClass('hide')
+//    } else {
+//      sandwhiches.addClass('hide')
+//      sandwhiches.removeClass('show')}
+//
+// if (ice-cream.checked == true) {
+//   ice-cream.addClass('show')
+//   ice-cream.removeClass('hide')
+//    } else {
+//      ice-cream.addClass('hide')
+//      ice-cream.removeClass('show')}
+//
+//   if (seafood.checked == true) {
+//     seafood.addClass('show')
+//     seafood.removeClass('hide')
+//      } else {
+//        seafood.addClass('hide')
+//        seafood.removeClass('show')}
+//
+// if (breakfast.checked == true) {
+//   breakfast.addClass('show')
+//   breakfast.removeClass('hide')
+//    } else {
+//      breakfast.addClass('hide')
+//      breakfast.removeClass('show')}
+//
+// if (steak.checked == true) {
+//   steak.addClass('show')
+//   steak.removeClass('hide')
+//    } else {
+//      steak.addClass('hide')
+//      steak.removeClass('show')}
+//
+// if (tacos.checked == true) {
+//   tacos.addClass('show')
+//   tacos.removeClass('hide')
+//    } else {
+//      tacos.addClass('hide')
+//      tacos.removeClass('show')}
+// }
